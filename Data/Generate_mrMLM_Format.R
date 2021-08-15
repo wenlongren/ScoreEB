3b63c1712f11093ed0723ed766438c6aec5d72b5
BStatistics <43625588+wenlongren@users.noreply.github.com>
2021-08-15 13:34:34 +0800

    Update Generate_mrMLM_Format.R

--- Data/Generate_mrMLM_Format.R
+++ Data/Generate_mrMLM_Format.R
@@ -2,7 +2,6 @@ library(BEDMatrix)
 library(data.table)
 
 dir1 <- "C:/Users/ThinkPad/Desktop/Lucky/ScoreEB/Frontiers_in_Genetics_Resubmit/Data/Sample_Size_Data/mrMLM_Format/2000/"
-dir2 <- "C:/Users/ThinkPad/Desktop/Lucky/ScoreEB/Frontiers_in_Genetics_Resubmit/Data/Sample_Size_Data/mrMLM_Format/2000/Pheno_2000/"
 
 file_sim_bed <- paste0(dir1, "Sim_Sample_2000.bed")
 file_sim_bim <- paste0(dir1, "Sim_Sample_2000.bim")
@@ -23,7 +22,7 @@ for(i in 1:len_sample){
 geno_for_code1 <- sim_bim[,5]
 mrMLM_geno <- cbind(sim_bim[,c(2,1,4)], geno_for_code1, t(sim_bed[,]))
 
-colnames(mrMLM_geno) <- c("rs#",	"chrom",	"pos",	"genotype for code 1", as.matrix(sim_fam[,1]))
+colnames(mrMLM_geno) <- c("rs#", "chrom", "pos", "genotype for code 1", as.matrix(sim_fam[,1]))
 
 ### compute kinship ###
 kin <- sim_bed%*%t(sim_bed)
@@ -34,14 +33,11 @@ rownames(kin) <- c(as.matrix(sim_fam[,1]))
 
 write.table(mrMLM_geno, paste0(dir1, "mrMLM_Genotype_2000.csv"), sep = ",", quote = FALSE, col.names = TRUE, row.names = FALSE)
 write.table(kin, paste0(dir1, "mrMLM_Kinship_2000.csv"), sep = ",", quote = FALSE, col.names = FALSE, row.names = TRUE)
-write.table(kin, paste0(dir1, "Kinship_2000.txt"), sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
 
-file_pheno <- paste0(dir2, "Sim.Pheno.10QTNs.2000.csv")
-pheno <- fread(file_pheno)
-new_pheno <- cbind(sim_fam[,1], pheno)
-colnames(new_pheno) <- c("<Phenotype>", colnames(pheno))
+pheno <- sim_fam[,-c(2:5)]
+colnames(new_pheno) <- c("<Phenotype>", paste("Trait",c(1:(dim(pheno)[2]-1))))
 
-write.table(new_pheno,paste0(dir1, "mrMLM_Phenotype_2000.csv"), sep = ",", quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(pheno,paste0(dir1, "mrMLM_Phenotype_2000.csv"), sep = ",", quote = FALSE, row.names = FALSE, col.names = TRUE)
 
 
 
